c5ad67a17e4666f54a891a6ddf8444abbaa4242d
Walter Heck <walter@tribily.com>
2012-01-11 10:38:59 +0200

    Added lucid backports repo for convenience

---
+++ manifests/repo/lucid-backports.pp
@@ -0,0 +1,15 @@
+class apt::repo::lucid-backports {
+
+  #Add lucid-backports repository
+  apt::key { "8347A27F": }
+  
+  apt::repository { "lucid-backports":
+    url => "http://archive.ubuntu.com/ubuntu",
+    distro => "${::lsbdistcodename}-backports",
+    repository => "main restricted universe multiverse",
+    require     => Apt::Key["8347A27F"],
+    source      => true
+  }
+  
+}
+
