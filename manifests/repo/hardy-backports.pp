30a60e70f1efa1587d3c400ee6c3a5695342b4b4
Walter Heck <walter@tribily.com>
2012-02-01 15:30:46 +0200

    Added hardy-backports

---
+++ manifests/repo/hardy-backports.pp
@@ -0,0 +1,15 @@
+class apt::repo::hardy-backports {
+
+  #Add hardy-backports repository
+  apt::key { "8347A27F": }
+  
+  apt::repository { "hardy-backports":
+    url => "http://archive.ubuntu.com/ubuntu",
+    distro => "${::lsbdistcodename}-backports",
+    repository => "main restricted universe multiverse",
+    require     => Apt::Key["8347A27F"],
+    source      => true
+  }
+  
+}
+
