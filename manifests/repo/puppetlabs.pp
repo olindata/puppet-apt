69f104239c7a5c4c6b620961af4bd56ebe5f8b41
Walter Heck <walter@tribily.com>
2012-02-01 12:21:19 +0200

    make the puppetabs module work for ubuntu as well

--- manifests/repo/puppetlabs.pp
+++ manifests/repo/puppetlabs.pp
@@ -4,9 +4,11 @@ class apt::repo::puppetlabs {
   apt::key { "4BD6EC30": 
     keyserver  => "pool.sks-keyservers.net",
   }
+  
+  $distid = inline_template("<%= scope.lookupvar('::lsbdistid').downcase %>") 
 
   apt::repository { "puppetlabs":
-    url        => "http://apt.puppetlabs.com/debian",
+    url        => "http://apt.puppetlabs.com/${::lsbdistid}",
     distro     => "${::lsbdistcodename}",
     repository => "main",
     require    => Apt::Key["4BD6EC30"],
