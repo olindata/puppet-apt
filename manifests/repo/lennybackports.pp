eb88dd38e05938b49cf96404b0d051cf1f2813c3
Walter Heck <walter@tribily.com>
2012-02-04 05:23:50 +0200

    replace key for debian backports by keyring package

--- manifests/repo/lennybackports.pp
+++ manifests/repo/lennybackports.pp
@@ -1,15 +1,19 @@
 class apt::repo::lennybackports {
 
   #Add lenny-backports repository
-  apt::key { "8347A27F":
-    keyserver  => "keyring.debian.org",
+#  apt::key { "8347A27F":
+#    keyserver  => "keyring.debian.org",
+#  }
+
+  package{ 'debian-backports-keyring':
+    ensure => present
   }
 
   apt::repository { "lenny-backports":
     url         => "http://backports.debian.org/debian-backports",
     distro      => "${::lsbdistcodename}-backports",
     repository  => "main",
-    require     => Apt::Key["8347A27F"],
+    require     => Package['debian-backports-keyring'],
     source      => true
   }
 }
