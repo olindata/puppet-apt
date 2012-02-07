47aaa99d7e3b657d3577f01649ffea86c298ecc5
Walter Heck <walter@tribily.com>
2012-02-07 04:14:01 +0200

    Add tribily.com repo's

---
+++ manifests/repo/tribily.pp
@@ -0,0 +1,49 @@
+class apt::repo::tribily {
+
+  $distribution = inline_template("<%= scope.lookupvar('::lsbdistid').downcase %>")
+
+  case $distribution {
+    'debian': {
+      case $::lsbdistcodename {
+        'lenny': {
+
+        }
+        'squeeze': {
+
+        }
+        default: {
+          err "Only debian lenny and squeeze are currently supported by apt::repo::tribily"
+        }
+      }
+    }
+    'ubuntu': {
+      case $::lsbdistcodename {
+        'hardy': {
+
+        }
+        'lucid': {
+
+        }
+        'natty': {
+
+        }
+        'oneiric': {
+
+        }
+        default: {
+          err "Only ubuntu hardy, lucid, natty and oneiric are currently supported by apt::repo::tribily"
+        }
+      }
+    }
+    default: {
+      err "Only debian and ubuntu are currently supported by apt::repo::tribily"
+    }
+  }
+
+  apt::repository { "tribily":
+    url        => "http://apt.tribily.com/${distribution}",
+    distro     => "${::lsbdistcodename}",
+    repository => "main",
+    source     => false
+  }
+}
---
+++ manifests/repo/tribilytesting.pp
@@ -0,0 +1,49 @@
+class apt::repo::tribilytesting {
+
+  $distribution = inline_template("<%= scope.lookupvar('::lsbdistid').downcase %>")
+
+  case $distribution {
+    'debian': {
+      case $::lsbdistcodename {
+        'lenny': {
+
+        }
+        'squeeze': {
+
+        }
+        default: {
+          err "Only debian lenny and squeeze are currently supported by apt::repo::tribilytesting"
+        }
+      }
+    }
+    'ubuntu': {
+      case $::lsbdistcodename {
+        'hardy': {
+
+        }
+        'lucid': {
+
+        }
+        'natty': {
+
+        }
+        'oneiric': {
+
+        }
+        default: {
+          err "Only ubuntu hardy, lucid, natty and oneiric are currently supported by apt::repo::tribilytesting"
+        }
+      }
+    }
+    default: {
+      err "Only debian and ubuntu are currently supported by apt::repo::tribilytesting"
+    }
+  }
+
+  apt::repository { "tribily-testing":
+    url        => "http://apt.tribily.com/${distribution}-testing",
+    distro     => "${::lsbdistcodename}-testing",
+    repository => "main",
+    source     => false
+  }
+}
