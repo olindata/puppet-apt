fd14971f657864b479646be4157b74f64efaf718
Walter Heck <walter@tribily.com>
2012-06-02 10:29:48 +0800

    Support multiple versions of mariadb

--- manifests/repo/mariadb.pp
+++ manifests/repo/mariadb.pp
@@ -1,17 +1,17 @@
-class apt::repo::mariadb {
+class apt::repo::mariadb($version = '5.2') {
 
   # Add puppetlabs repository
-  apt::key { "1BB943DB" :
-    keyserver => "keyserver.ubuntu.com",
+  apt::key { '1BB943DB' :
+    keyserver => 'keyserver.ubuntu.com',
   }
 
   #deb http://mirror2.hs-esslingen.de/mariadb/repo/5.2/debian lenny main
   #deb-src http://mirror2.hs-esslingen.de/mariadb/repo/5.2/debian lenny main
-  apt::repository { "mariadb" :
-    url         => "http://ftp.osuosl.org/pub/mariadb/repo/5.2/debian",
+  apt::repository { 'mariadb' :
+    url         => "http://ftp.osuosl.org/pub/mariadb/repo/${version}/debian",
     distro      => $::lsbdistcodename,
-    repository  => "main",
-    require     => Apt::Key["1BB943DB"],
+    repository  => 'main',
+    require     => Apt::Key['1BB943DB'],
     source      => true,
   }
 }
