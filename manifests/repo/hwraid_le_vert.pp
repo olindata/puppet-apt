ae3c799c79ab5c1435ca625ce213842c2219d68d
Walter Heck <walterheck@olindata.com>
2013-08-09 00:25:37 +0200

    hwraid finally has a gpg key, yay!

--- manifests/repo/hwraid_le_vert.pp
+++ manifests/repo/hwraid_le_vert.pp
@@ -1,5 +1,9 @@
 class apt::repo::hwraid_le_vert {
 
+  apt::key { 'hwraid_le_vert':
+    url => 'http://hwraid.le-vert.net/debian/hwraid.le-vert.net.gpg.key'
+  }
+  
   apt::repository { 'hwraid':
     url         => 'http://hwraid.le-vert.net/debian',
     distro      => $::lsbdistcodename,
